3dc3330c7a08873c486cfd97b49b3c9b34fefba6
Yasunari t <u7df25c588m737@gmail.com>
2020-11-12 19:23:19 +0900

    init: google map lib

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import * as VueGoogleMaps from "vue2-google-maps";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "",
+    libraries: "places",
+  },
+});
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
